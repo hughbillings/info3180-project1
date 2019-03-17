e97a8e88b97cb23e9bab24c0cc77c3bbd342f335
Hugh Billings <hugh.billings@mymona.uwi.edu>
2019-03-17 23:15:32 +0000

    Trackw

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import math
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "this is a super secure key"  # you should mae this more random and unique
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://yecsapnzlttgcb:8860d3512549e3ebba04aa68ede74496e30041ff458ea1d46d7c4ed7f5402af0@ec2-54-221-244-196.compute-1.amazonaws.com:5432/d9d6gbbcjoc71g"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lobvwtloxzfgak:ea9bcec6f62112652616cbc60bc8cdd543d57a07a1cc71b07436bca22ab72388@ec2-54-221-236-144.compute-1.amazonaws.com:5432/d5768p723rjbsq"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
 UPLOAD_FOLDER='./app/static/uploads'
 
